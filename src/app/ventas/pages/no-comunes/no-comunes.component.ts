7175a91bc344439444e1f11fde67ebf77a123a4f
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-12 15:10:21 -0600

    Pipes i18nSelect e i18nPlural

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
@@ -6,11 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
+  nombre: string = "Roberto";
+  genero: string = "masculino";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  // i18Select
+  invitacionMapa = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
   }
 
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Roberto', 'Fernando']
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    'other': 'tenemos # clientes esperando.'
+  }
+
+  cambiarCliente() {
+    this.nombre = "Maria";
+    this.genero = "femenino";
+  }
+
+  eliminarCliente() {
+    this.clientes.shift();
+  }
 }
