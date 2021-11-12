1cacf4f268e15e2a875b0c7e5449b0ba0774463e
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-11 21:33:10 -0600

    Configurando router basico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
 import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRouterModule,
     SharedModule,
   ],
   providers: [],
