56e475efcd823c87dd6a47cdc0d496140c78fd44
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-12 11:40:25 -0600

    Importacion de locales global y de uso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -7,6 +7,13 @@ import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
 import { VentasModule } from './ventas/ventas.module';
 
+// Cambiar el locale de la app
+import localEsMx from '@angular/common/locales/es-MX';
+import localFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localEsMx);
+registerLocaleData(localFr);
 @NgModule({
   declarations: [
     AppComponent
@@ -17,7 +24,9 @@ import { VentasModule } from './ventas/ventas.module';
     SharedModule,
     VentasModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID, useValue: 'es-MX'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
