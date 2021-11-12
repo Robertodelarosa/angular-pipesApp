9ce570dd8476fd05b76839f66c3debd0bc806513
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-12 16:49:27 -0600

    Implementando Pipe mayusculas

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 
-  constructor() { }
+  enMayusculas: boolean = true;
 
-  ngOnInit(): void {
+  cambiarMayusculas() {
+    this.enMayusculas = !this.enMayusculas;
   }
-
 }
