5c86622c83db542ff4074e95b7e6eae07a5cd437
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-11 20:36:14 -0600

    Modularizacion de Modulos PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modulo de importaciónes de prime-ng
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
